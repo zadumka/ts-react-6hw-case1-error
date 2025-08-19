e23fccb50a0980bf41f6dbbcd637b57e14f3dc5e
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-19 12:33:57 +0300

    Update page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,11 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import NotesClient from './Notes.client';
 import { fetchNotes } from '@/lib/api';
 
 export default async function NotesPage() {
   const queryClient = new QueryClient();
 
- 
-  queryClient.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ['notes'],
     queryFn: () => fetchNotes({
       search: "",
@@ -14,9 +13,8 @@ export default async function NotesPage() {
     }),
   });
 
+ 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient />
-    </HydrationBoundary>
+    <NotesClient />
   );
 }
